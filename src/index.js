369983be15d09297e1573ae30d2e87a420cc9c5e
Mac <mac@MacdeMBP.(none)>
2024-07-17 10:19:51 +0800

    docs: mac忽略 ._开头文件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import PaintEditor from './containers/paint-editor.jsx';
 import ScratchPaintReducer from './reducers/scratch-paint-reducer';
-
+console.log("mammoth coding scratch-paint!");
 export {
     PaintEditor as default,
     ScratchPaintReducer
